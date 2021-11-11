cd1b3a1f1975ae020d1caed813499c8e2a5fd93a
Matt Casperson <mc.casperson@gmail.com>
2021-11-10 16:14:31 -0800

    add third test for Character
    Co-authored-by: Kate Kiatsiri <keidsiri@gmail.com>
    Co-authored by: Kim Brannian  <brannian@gmail.com>

--- __tests__/test.js
+++ __tests__/test.js
@@ -3,14 +3,21 @@ import Character from './../src/js/business.js';
 describe('Character', () => {
 
   test('should correctly create a character object with a name, a type, and a level', () => {
-    const myCharacter = new Character('Gandalf', 'Wizard', '9');
+    const myCharacter = new Character('Gandalf', 'Wizard', 9);
     expect(myCharacter.name).toEqual('Gandalf');
     expect(myCharacter.type).toEqual('Wizard');
-    expect(myCharacter.level).toEqual('9');
+    expect(myCharacter.level).toEqual(9);
   });
   test("Should display charector information with name, type and level", () => {
-    const myCharacter = new Character('Gandalf', 'Wizard', '9');
+    const myCharacter = new Character('Gandalf', 'Wizard', 9);
     expect(myCharacter.characterInfo()).toEqual("Your character is Gandalf, he is a great Wizard at level 9!");
   });
-});
+  test("Should add one to character's current level", () => {
+    const myCharacter = new Character('Gandalf', 'Wizard', 9);
+    expect(myCharacter.levelUp()).toEqual(10);
+  });
+})
 
+
+
+// this.level += 1;
